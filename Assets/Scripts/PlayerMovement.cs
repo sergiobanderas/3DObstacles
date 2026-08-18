17210958f88083f7b0b8fb08ac55e68ace9309a4
sergiobanderas <sergio.banderas@fpalanturing.es>
2026-08-18 18:53:36 +0200

    Variables and Translate

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float xValue = 0.01f;
+    float yValue = 0.01f;
+    float zValue = 0.01f;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);        
+    }
+}
