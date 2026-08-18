bfde48118fd5bfea2900a6d3a35f6fa2cc499e19
sergiobanderas <sergio.banderas@fpalanturing.es>
2026-08-18 19:07:41 +0200

    Use SerializeField

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float xValue = 0.01f;
-    float yValue = 0.01f;
-    float zValue = 0.01f;
-    
+    [SerializeField] private float xValue = 0.01f;
+    [SerializeField] private float yValue = 0.01f;
+    [SerializeField] private float zValue = 0.01f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
